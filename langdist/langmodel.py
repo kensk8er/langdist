fixed a bug in computation of loss (padding was included)

langdist/langmodel.py
@@ -4,11 +4,10 @@ This module implements language modeling algorithms.
 """
 from copy import deepcopy
 
-import tensorflow as tf
 import numpy as np
-from tensorflow.contrib.rnn import DropoutWrapper
-from tensorflow.contrib.rnn import LSTMCell
-from tensorflow.contrib.rnn import MultiRNNCell
+import tensorflow as tf
+from tensorflow.contrib.rnn import DropoutWrapper, LSTMCell, MultiRNNCell
+from tensorflow.contrib.seq2seq import sequence_loss
 
 from langdist.batch import BatchGenerator
 from langdist.encoder import CharEncoder
@@ -63,7 +62,8 @@ class CharLSTM(object):
                 _, loss = session.run(
                     [nodes['optimizer'], nodes['loss']],
                     feed_dict={nodes['X']: X_batch, nodes['Y']: Y_batch,
-                               nodes['seq_lens']: seq_lens, nodes['dropout_prob']: 0.5})
+                               nodes['seq_lens']: seq_lens, nodes['dropout_prob']: 0.5,
+                               nodes['batch_size']: batch_size})
                 losses.append(loss)
 
                 if batch_id > 0 and batch_id % stat_interval == 0:
@@ -102,6 +102,7 @@ class CharLSTM(object):
                 nodes['Y'] = tf.placeholder(tf.int32, [None, None], name='Y')
                 nodes['seq_lens'] = tf.placeholder(tf.int32, [None], name='seq_lens')
                 nodes['dropout_prob'] = tf.placeholder(tf.float32, shape=[], name='dropout_prob')
+                nodes['batch_size'] = tf.placeholder(tf.int32, shape=[], name='batch_size')
 
             with tf.name_scope('embedding_layer') as name_scope:
                 nodes['embeddings'] = tf.Variable(
@@ -123,16 +124,15 @@ class CharLSTM(object):
                 nodes['W_s'] = tf.Variable(
                     tf.random_normal([self._rnn_size, self._vocab_size]), name='weight')
                 nodes['b_s'] = tf.Variable(tf.random_normal([self._vocab_size]), name='bias')
-
-                # reshape y so we can get the logits in a single matmul
-                Y_reshaped = tf.reshape(nodes['Y'], [-1])
                 logits = tf.matmul(rnn_outputs, nodes['W_s']) + nodes['b_s']
 
             with tf.variable_scope('loss'):
-                # TODO: need to mask the loss (many entries are padding)
-                nodes['loss'] = tf.reduce_mean(
-                    tf.nn.sparse_softmax_cross_entropy_with_logits(
-                        logits=logits, labels=Y_reshaped))
+                # reshape the logits back to batch_size * seq_lens such that we can compute mean
+                # loss after masking padding inputs easily by using sequence_loss
+                max_seq_len = tf.reduce_max(nodes['seq_lens'])
+                logits = tf.reshape(logits, [nodes['batch_size'], max_seq_len, -1])
+                weights = tf.cast(tf.sequence_mask(nodes['seq_lens'], max_seq_len), tf.float32)
+                nodes['loss'] = sequence_loss(logits=logits, targets=nodes['Y'], weights=weights)
                 nodes['optimizer'] = tf.train.AdamOptimizer(self._learning_rate).minimize(
                     nodes['loss'])
 