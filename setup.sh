e22d66121e9684a1ec18cebd0291a5fb740687aa
kensk8er <kensk8er1017@gmail.com>
2017-03-14 14:53:24 +0000

    added tensorflow install in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
-conda create -n langdist python=3.5 tensorflow -y
+conda create -n langdist python=3.5 pip ipython -y
+source activate langdist
+pip install --ignore-installed --upgrade https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-1.0.1-cp35-cp35m-linux_x86_64.whl
+
