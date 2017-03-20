caf78be8d2be658ad545f14af4996b01f35a801a
kensk8er <kensk8er1017@gmail.com>
2017-03-20 10:50:31 +0000

    fixed an issue in zh texts (un-natural white spaces)

--- langdist/preprocess.py
+++ langdist/preprocess.py
@@ -3,8 +3,9 @@
 This module is used to preprocess corpora.
 """
 import os
-
 import pickle
+
+import regex
 
 from langdist import PACKAGE_ROOT
 from langdist.util import CorpusParser
@@ -14,9 +15,15 @@ __author__ = 'kensk8er1017@gmail.com'
 _PROCESSED_CORPUS_DIR = os.path.join(PACKAGE_ROOT, os.path.pardir, 'corpora')
 
 
-def _preprocess(paragraph):
+def _preprocess(paragraph, locale):
     """Preprocess a paragraph."""
-    return paragraph.strip()
+    paragraph = paragraph.strip()
+
+    # for some reason, zh text has white spaces between characters, which isn't normal for zh texts
+    if locale == 'zh':
+        paragraph = regex.sub(r'\s', '', paragraph)
+
+    return paragraph
 
 
 def preprocess_corpus(locale):
@@ -28,7 +35,7 @@ def preprocess_corpus(locale):
     corpus = list()
     parser = CorpusParser(locale)
     for paragraph in parser.gen_paragraphs():
-        paragraph = _preprocess(paragraph)
+        paragraph = _preprocess(paragraph, locale)
         corpus.append(paragraph)
 
     processed_filepath = os.path.join(_PROCESSED_CORPUS_DIR, '{}.pkl'.format(locale))
