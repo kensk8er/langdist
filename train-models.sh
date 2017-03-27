f74e0c7a63400bcfc3563f2e149b081d7b39df15
kensk8er <kensk8er1017@gmail.com>
2017-03-27 01:10:09 +0100

    minor change

--- train-models.sh
+++ train-models.sh
@@ -3,21 +3,23 @@
 source activate langdist
 
 # train initial language models
-python langdist/trainer.py train en --patience=30000
-python langdist/trainer.py train fr --patience=30000
-python langdist/trainer.py train de --patience=30000
-python langdist/trainer.py train ja --patience=30000
-python langdist/trainer.py train zh --patience=30000
-python langdist/trainer.py train ar --patience=30000
+python langdist/trainer.py train en
+python langdist/trainer.py train fr
+python langdist/trainer.py train de
+python langdist/trainer.py train ja
+python langdist/trainer.py train zh
+python langdist/trainer.py train ar
+python langdist/trainer.py train pt
+python langdist/trainer.py train ru
 
 # train other language models on top of initial models
-python langdist/trainer.py retrain en fr --patience=30000
-python langdist/trainer.py retrain fr en --patience=30000
-python langdist/trainer.py retrain en de --patience=30000
-python langdist/trainer.py retrain de en --patience=30000
-python langdist/trainer.py retrain en ja --patience=30000
-python langdist/trainer.py retrain ja en --patience=30000
-python langdist/trainer.py retrain ja zh --patience=30000
-python langdist/trainer.py retrain zh ja --patience=30000
-python langdist/trainer.py retrain en ar --patience=30000
-python langdist/trainer.py retrain ar en --patience=30000
+python langdist/trainer.py retrain en fr
+python langdist/trainer.py retrain fr en
+python langdist/trainer.py retrain en de
+python langdist/trainer.py retrain de en
+python langdist/trainer.py retrain en ja
+python langdist/trainer.py retrain ja en
+python langdist/trainer.py retrain ja zh
+python langdist/trainer.py retrain zh ja
+python langdist/trainer.py retrain en ar
+python langdist/trainer.py retrain ar en
