e3920aacf9c40589f31d90508c81bca7531d43dd
kensk8er <kensk8er1017@gmail.com>
2017-03-27 01:23:06 +0100

    removed ru, added ind

--- download-corpora.sh
+++ download-corpora.sh
@@ -7,8 +7,9 @@ wget -O corpora/ja.xml https://raw.githubusercontent.com/christos-c/bible-corpus
 wget -O corpora/fr.xml https://raw.githubusercontent.com/christos-c/bible-corpus/master/bibles/French.xml
 wget -O corpora/de.xml https://raw.githubusercontent.com/christos-c/bible-corpus/master/bibles/German.xml
 wget -O corpora/pt.xml https://raw.githubusercontent.com/christos-c/bible-corpus/master/bibles/Portuguese.xml
-wget -O corpora/ru.xml https://raw.githubusercontent.com/christos-c/bible-corpus/master/bibles/Russian.xml
+#wget -O corpora/ru.xml https://raw.githubusercontent.com/christos-c/bible-corpus/master/bibles/Russian.xml
 wget -O corpora/ar.xml https://raw.githubusercontent.com/christos-c/bible-corpus/master/bibles/Arabic.xml
+wget -O corpora/ind.xml https://raw.githubusercontent.com/christos-c/bible-corpus/master/bibles/Indonesian.xml
 #wget -O corpora/ko.xml https://raw.githubusercontent.com/christos-c/bible-corpus/master/bibles/Korean.xml
 #wget -O corpora/hi.xml https://raw.githubusercontent.com/christos-c/bible-corpus/master/bibles/Hindi.xml
 #wget -O corpora/tr.xml https://raw.githubusercontent.com/christos-c/bible-corpus/master/bibles/Turkish.xml
--- langdist/constant.py
+++ langdist/constant.py
@@ -8,6 +8,6 @@ from langdist import PACKAGE_ROOT
 
 __author__ = 'kensk8er1017@gmail.com'
 
-LOCALES = ['en', 'zh', 'ja', 'fr', 'de', 'pt', 'ru', 'ar']
+LOCALES = ['en', 'zh', 'ja', 'fr', 'de', 'pt', 'ind', 'ar']
 CORPUS_DIR = os.path.join(PACKAGE_ROOT, os.path.pardir, 'corpora')
 MODEL_DIR = os.path.join(PACKAGE_ROOT, os.path.pardir, 'models')
