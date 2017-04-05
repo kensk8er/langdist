43a25fb89488889fe857fa825022046501a995e0
kensk8er <kensk8er1017@gmail.com>
2017-04-05 01:10:26 +0100

    changed default value for final_dropout

--- langdist/langmodel.py
+++ langdist/langmodel.py
@@ -34,7 +34,7 @@ class CharLSTM(object):
     _tensorboard_dir = 'tensorboard.log'
 
     def __init__(self, embedding_size=128, rnn_size=256, num_rnn_layers=2, learning_rate=0.001,
-                 rnn_dropouts=None, final_dropout=0.5, encoder=None):
+                 rnn_dropouts=None, final_dropout=1.0, encoder=None):
         # in order to avoid using mutable object as a default argument
         if rnn_dropouts is None:
             # default is 1.0, which means no dropout
