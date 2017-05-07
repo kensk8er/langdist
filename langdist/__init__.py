be205dcd5e4213a3fce7691c7a0db04f44e93295
kensk8er <kensk8er1017@gmail.com>
2017-05-07 15:07:22 +0100

    added the version number

--- langdist/__init__.py
+++ langdist/__init__.py
@@ -4,6 +4,8 @@ langdict package
 """
 import os
 
-__author__ = 'kensuke@complyadvantage.com'
+from langdist._version import __version__
+
+__author__ = 'kensk8er1017@gmail.com'
 
 PACKAGE_ROOT = os.path.dirname(os.path.realpath(__file__))
