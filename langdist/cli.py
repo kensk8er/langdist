88e3baf387ec8d137f237247bbb0ae18799c40d6
kensk8er <kensk8er1017@gmail.com>
2017-05-21 19:39:27 +0100

    implemented download-bible sub-command, created the mapping from langauge code to language

--- langdist/cli.py
+++ langdist/cli.py
@@ -3,7 +3,7 @@
 Command Line Interface (CLI) of langdist package.
 
 Usage:
-    langdist preprocess <input-corpus-path> <output-corpus-path>
+    langdist download-bible <lang-code> <output-corpus-path>
     langdist transliterate <input-corpus-path> <lang-code> <output-corpus-path>
     langdist fit-encoder <encoder-path> <input-corpus-path>...
     langdist train <input-corpus-path> <encoder-path> <model-path> [options]
@@ -12,12 +12,20 @@ Usage:
     langdist -v | --version
 
 Commands:
-    preprocess  Preprocess a corpus downloaded from http://christos-c.com/bible/ and store it into a .pkl file
-    transliterate  Transliterate a preprocessed corpus and store it into a .pkl file
+    download-bible  Download a bible corpus from http://christos-c.com/bible/ and store it into a .pkl file after preprocessing
+    transliterate  Transliterate a corpus and store it into a .pkl file
     fit-encoder  Fit an encoder on 1 or more corpora and save it to a .pkl file
     train  Train a language model from the scratch (monolingual model)
     retrain  Train a language model from another language model (bilingual model)
 
+Arguments:
+    input-corpus-path  path to the corpus file you want to process
+    output-corpus-path  path to where you save(d) the generated corpus 
+    encoder-path  path to where you save the fitted encoder
+    lang-code  language code (2 characters) of the corpus you want to transliterate (e.g. ar, ja, zh)
+    model-path  path to the model directory you where your model will be saved
+    old-model-path  path to the model directory of a language model which you want to train a new language model from (only required for `retrain` command)
+    
 Options:
     -h --help  Show this screen
     -v --version  Show version
@@ -26,46 +34,53 @@ Options:
     --log-path=<str>  If specified, log into the file at the path [default: ]
     --verbose  Show debug messages
 
-Arguments:
-    input-corpus-path  path to the corpus file you want to process
-    output-corpus-path  path to where you save the generated corpus 
-    encoder-path  path to where you save the fitted encoder
-    lang-code  language code (2 characters) of the corpus you want to transliterate (e.g. ar, ja, zh)
-    model-path  path to the model directory you where your model will be saved
-    old-model-path  path to the model directory of a language model which you want to train a new language model from (only required for `retrain` command)
-
 Examples:
-    langdist preprocess en_corpus.xml en_corpus.pkl
+    langdist download-bible en en_corpus.pkl
     langdist transliterate ja_corpus.pkl ja transliterated_ja_corpus.pkl
     langdist fit-encoder encoder.pkl en_corpus.pkl ja_corpus.pkl zh_corpus.pkl ar_corpus.pkl
     langdist train en_corpus.pkl encoder.pkl en_model --patience=819200 --logpath=langdist.log
     langdist retrain en_model encoder.pkl fr_corpus.pkl en2fr_model --patience=819200 --logpath=langdist.log
 
 """
+import json
 import os
 import shutil
 import logging
 import pickle
+from urllib.request import urlretrieve
 
 from docopt import docopt
 
 from langdist import __version__
-from langdist.transliterator import get_transliterator
+from langdist.constant import LANG_CODE2LANGUAGE
 from langdist.util import get_logger, set_default_log_path, set_default_log_level, set_log_level, \
     set_log_path
 from langdist.preprocess import preprocess_corpus
+
+_BIBLE_CORPUS_URL = 'https://raw.githubusercontent.com/christos-c/bible-corpus/master/bibles/{}.xml'
 
 _LOGGER = get_logger(__name__)
 
 __author__ = 'kensk8er1017@gmail.com'
 
 
-def preprocess(input_corpus_path, output_corpus_path):
+def download_bible(lang_code, output_corpus_path, keep_xml=False):
     """
-    Preprocess a Multilingual Bible Parallel Corpus downloaded from http://christos-c.com/bible/
-    and save it into a pickle file.
+    Download a bible corpus from Multilingual Bible Parallel Corpus (http://christos-c.com/bible/),
+    perform preprocessing, and save it into a pickle file.
     """
-    preprocess_corpus(input_corpus_path, output_corpus_path)
+    xml_path = os.path.join(os.path.dirname(output_corpus_path), '{}.xml'.format(lang_code))
+    try:
+        language = LANG_CODE2LANGUAGE[lang_code]
+    except KeyError:
+        _LOGGER.error('lang_code={} is not supported. The following lang_codes are supported for '
+                      'the corresponding languages:\n{}'
+                      .format(lang_code, json.dumps(LANG_CODE2LANGUAGE, indent=2)))
+        raise
+    urlretrieve(_BIBLE_CORPUS_URL.format(language), xml_path)
+    preprocess_corpus(xml_path, output_corpus_path)
+    if not keep_xml:
+        os.remove(xml_path)
 
 
 def transliterate(input_corpus_path, lang_code, transliterated_corpus_path):
@@ -73,6 +88,7 @@ def transliterate(input_corpus_path, lang_code, transliterated_corpus_path):
     Transliterate the text of the given corpus into latin alphabets. `lang_code` needs to be the one
     that is supported by `langdist.transliterator` module.
     """
+    from langdist.transliterator import get_transliterator  # import locally because it's slow
     with open(input_corpus_path, 'rb') as input_corpus_file:
         input_corpus = pickle.load(input_corpus_file)
     transliterator = get_transliterator(lang_code)
@@ -120,8 +136,8 @@ def main():
 
     _LOGGER.debug('Configuration:\n{}'.format(args))
 
-    if args['preprocess']:
-        preprocess(args['<input-corpus-path>'], args['<output-corpus-path>'])
+    if args['download-bible']:
+        download_bible(args['<lang-code>'], args['<output-corpus-path>'])
         return
 
     if args['transliterate']:
